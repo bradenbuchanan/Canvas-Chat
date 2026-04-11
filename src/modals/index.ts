9a442eac47ed3ffad102b2b2d1c9098011376679
Braden Buchanan <bradenjbuchanan@gmail.com>
2026-04-10 21:07:33 -0400

    barrel re-export.

---
+++ src/modals/index.ts
@@ -0,0 +1,4 @@
+export { PromptEditorModal } from "./PromptEditorModal";
+export { ExpandedChatModal } from "./ExpandedChatModal";
+export { SettingsModal } from "./SettingsModal";
+export type { SettingsHost } from "./SettingsModal";
